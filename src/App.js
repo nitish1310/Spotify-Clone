213ad7ff393e66e50272624b46a7eccfafcfb1ab
nitish1391 <nsoman@my.bridgeport.edu>
2020-10-15 22:49:13 -0400

    Version Update
    Version Update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { useState } from "react";
 import "./App.css";
 import Login from "./Login";
 import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
@@ -16,15 +20,17 @@ function App() {
 
     if (_token) {
       setToken(_token);
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        console.log("🧔", user);
+      });
     }
-    console.log("I have a token :point >>", token);
+    console.log("I have a token 👉 >>", token);
   }, []);
 
-  return (
-    <div className="app">
-      <Login />
-    </div>
-  );
+  return <div className="app">{token ? <Player /> : <Login />}</div>;
 }
 
 export default App;
