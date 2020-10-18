342782c040ad465fcd87d94e2b5c7e35460bde50
nitish1310 <nitish.soman13@gmail.com>
2020-10-18 11:13:51 -0400

    Version Update
    Version Update

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -17,6 +17,10 @@ function Sidebar() {
       <SidebarOption Icon={HomeIcon} title="Home" />
       <SidebarOption Icon={SearchIcon} title="Search" />
       <SidebarOption Icon={LibraryMusicIcon} title="Your  Library" />
+
+      <br />
+      <strong className="sidebar__title">PLAYLISTS</strong>
+      <hr />
     </div>
   );
 }
